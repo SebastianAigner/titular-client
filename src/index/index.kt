b1c64560a07e6b9e3d72db86715a2c9a29f7ed75
Sebastian Aigner <sebastian.aigner@tum.de>
2018-09-03 20:13:31 +0200

    learn to read

--- src/index/index.kt
+++ src/index/index.kt
@@ -57,7 +57,7 @@ class App(props: AppProps): RComponent<AppProps, AppState>(props) {
     var timerId: Int? = null
 
     override fun AppState.init(props: AppProps) {
-        val addr = js("process.env.API_WEBSOCKET_ADDRESS")
+        val addr = js("process.env.REACT_APP_API_WEBSOCKET_ADDRESS")
         println("Working with API at $addr")
         val finalAddr = addr as? String ?: "ws://0.0.0.0:8080/myws/echo"
         socket = WebSocket("ws://0.0.0.0:8080/myws/echo")
